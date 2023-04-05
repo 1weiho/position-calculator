17977049be1ea3a305cf0bc2e40761cc7c7b5227
David Ho <davidho0403@gmail.com>
2023-04-06 02:54:09 +0800

    fix: resize the vertical margin

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,17 +1,17 @@
-import { Github, TrendingUp, TrendingDown, Send } from "lucide-react";
+import { Github, TrendingUp, TrendingDown, CalculatorIcon } from "lucide-react";
 
 export default function Calculator() {
   return (
     <>
-      <div className="w-screen flex justify-end px-8 mt-8 mb-12 relative">
+      <div className="w-screen flex justify-end px-8 mt-8 mb-8 relative">
         <div className="h-96 w-96 bg-gradient-to-r from-sky-500 to-sky-200 absolute rounded-full blur-3xl opacity-40 left-0 top-0 -ml-40 -mt-40 -z-10"></div>
         <Github />
       </div>
-      <div className="w-screen px-12 mb-12 space-y-2">
+      <div className="w-screen px-12 mb-8 space-y-2">
         <h1 className="text-nord0 font-black text-xl tracking-widest">合約倉位計算器</h1>
         <h2 className="text-nord3 text-xs tracking-widest">以最大承受風險來回推倉位保證金大小</h2>
       </div>
-      <div className="w-screen px-20 flex justify-between mb-12">
+      <div className="w-screen px-20 flex justify-between mb-8">
         <button className="flex items-center justify-center space-x-2 text-white bg-nord9 px-5 py-3 rounded-lg">
           <TrendingUp className="h-4 w-4" />
           <p className="text-xs font-semibold">做 多</p>
@@ -21,7 +21,7 @@ export default function Calculator() {
           <p className="text-xs font-semibold">做 多</p>
         </button>
       </div>
-      <div className="w-screen flex flex-col px-14 space-y-7">
+      <div className="w-screen flex flex-col px-14 space-y-6">
         <div className="space-y-1">
           <label className="text-nord3 text-xs tracking-widest">最高承受風險（USDT）</label>
           <input className="w-full bg-nord6 rounded-lg h-12" type="text" />
@@ -45,8 +45,8 @@ export default function Calculator() {
           </div>
         </div>
         <button className="flex items-center space-x-2 text-white bg-nord7 px-5 py-3 rounded-lg justify-center">
-          <Send className="h-4 w-4" />
-          <p className="text-xs font-semibold">產 生</p>
+          <CalculatorIcon className="h-4 w-4" />
+          <p className="text-xs font-semibold">計 算</p>
         </button>
       </div>
     </>
