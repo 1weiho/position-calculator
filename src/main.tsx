f8f058e4b79f57fc9c58cac20298f60cdc61a53b
David Ho <davidho0403@gmail.com>
2023-04-12 03:42:22 +0800

    feat: add react-ga for analyzing

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-JMJXNDDQEY');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
